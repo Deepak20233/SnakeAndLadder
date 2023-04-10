d3748ba7f45fbd4774828d3196d1b0a023fd9947
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-10 20:46:27 +0530

    Added:Functionality to get Dice value

---
+++ SnakeAndLadderUC2.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class SnakeAndLadderUC2 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to snake and ladder project");
+        int startPosition = 0;
+        int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+
+	}
+
+}
