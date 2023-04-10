2b770ea881eeeb86c23822044bc6130eaca01f44
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-10 20:38:38 +0530

    Added: Starting position

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class SnakeAndLadderUC1 {
+    
+    public static void main(String[] args) {
+    	 System.out.println("Welcome to snake and ladder Game");
+         int startPosition = 0;
+         
+     }
+ 
+	    }
+    
